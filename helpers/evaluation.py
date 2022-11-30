5b70533cf39b3c46d91eb37050f6c619ebc85cea
Radha <radha.mastandrea@gmail.com>
2022-11-30 14:17:49 -0800

    added kfold classifier w/ dataloader

--- helpers/evaluation.py
+++ helpers/evaluation.py
@@ -15,6 +15,7 @@ from sklearn.metrics import roc_auc_score, roc_curve
 
 from sklearn.utils import class_weight
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import KFold
 
 
 from tqdm import tqdm
@@ -406,6 +407,184 @@ def analyze_band_transform(dir_to_save, idd, train_samp_1, train_samp_2, test_sa
         auc = 1.0 - auc
     
     return auc
+
+
+def analyze_band_transform_kfold(dir_to_save, idd, train_samp_1, train_samp_2, test_samp_1, test_samp_2, n_features, n_epochs, batch_size, lr, patience, device, early_stop = True, visualize = True, seed = None, k_folds = 5):
+    
+    if seed is not None:
+        #print(f"Using seed {seed}...")
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+    
+    
+    # transformed SIM has label 0, DAT has label 1
+    # make the input and output data
+    X_train = np.concatenate((train_samp_1, train_samp_2))
+    y_train = np.concatenate((torch.zeros((train_samp_1.shape[0], 1)), torch.ones((train_samp_2.shape[0],1))))    
+    
+    # get weights in case we're oversampling
+    class_weights = class_weight.compute_class_weight('balanced', np.unique(y_train.reshape(-1)), y_train.reshape(-1))
+    class_weights = dict(enumerate(class_weights))   
+    
+    X_test = np.concatenate((test_samp_1, test_samp_2))
+    y_test = np.concatenate((torch.zeros((test_samp_1.shape[0], 1)), torch.ones((test_samp_2.shape[0],1))))
+    
+    print("Train data, labels shape:", X_train.shape, y_train.shape)
+    print("Test data, labels  shape:", X_test.shape, y_test.shape)
+
+    # send to device
+    X_train = np_to_torch(X_train, device)
+    X_test = np_to_torch(X_test, device)
+    y_train = np_to_torch(y_train, device)
+    
+    # Define the K-fold Cross Validator
+    kfold = KFold(n_splits=k_folds, shuffle=True)
+    fold_best_val_losses = []
+    
+    # K-fold Cross Validation model evaluation
+    for fold, (train_ids, val_ids) in enumerate(kfold.split(X_train)):
+    
+        # Print
+        print(f'FOLD {fold}')
+        print('--------------------------------')
+
+        X_train_fold = X_train[train_ids]
+        y_train_fold = y_train[train_ids]
+        
+        X_val_fold = X_train[val_ids]
+        y_val_fold = y_train[val_ids]
+        
+        train_set = torch.utils.data.TensorDataset(X_train_fold, y_train_fold)
+        val_set = torch.utils.data.TensorDataset(X_val_fold, y_val_fold)
+        train_loader = torch.utils.data.DataLoader(train_set, batch_size = batch_size, shuffle = True)
+        val_loader = torch.utils.data.DataLoader(val_set, batch_size = batch_size, shuffle = True)
+        
+        # initialze the network
+        dense_net = NeuralNet(input_shape = n_features)
+        criterion = F.binary_cross_entropy 
+        optimizer = torch.optim.Adam(dense_net.parameters(), lr=lr)
+        dense_net.to(device)
+        
+        if early_stop:
+            early_stopping = EarlyStopping(patience=patience)
+        
+         # save the best model
+        val_loss_to_beat = 10000
+        best_epoch = -1
+
+        epochs, losses, losses_val = [], [], []
+
+        for epoch in tqdm(range(n_epochs)):
+            losses_batch_per_e = []
+            # batching    
+            for batch_index, (batch_data, batch_labels) in enumerate(train_loader):
+
+                # calculate the loss, backpropagate
+                optimizer.zero_grad()
+
+                batch_data = X_train_fold[indices]
+                batch_labels = y_train_fold[indices]
+
+                # get the weights
+                batch_weights = (torch.ones(batch_labels.shape, device=device)
+                            - batch_labels)*class_weights[0] \
+                            + batch_labels*class_weights[1]
+
+                loss = criterion(dense_net(batch_data), batch_labels, weight = batch_weights)
+
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
+                losses_batch_per_e.append(loss.detach().cpu().numpy())
+
+            epochs.append(epoch)
+            losses.append(np.mean(losses_batch_per_e))
+
+            # validation
+            with torch.no_grad():
+                val_losses_batch_per_e = []
+                
+                for batch_index, (batch_data, batch_labels) in enumerate(val_loader):
+                    # calculate the loss, backpropagate
+                    optimizer.zero_grad()
+
+                    batch_data = X_val_fold[indices]
+                    batch_labels = y_val_fold[indices]
+
+                    # get the weights
+                    batch_weights = (torch.ones(batch_labels.shape, device=device)
+                                - batch_labels)*class_weights[0] \
+                                + batch_labels*class_weights[1]
+
+                    val_loss = criterion(dense_net(batch_data), batch_labels, weight = batch_weights) 
+                    val_losses_batch_per_e.append(val_loss.detach().cpu().numpy())
+
+                losses_val.append(np.mean(val_losses_batch_per_e))
+
+                # see if the model has the best val loss
+                if np.mean(val_losses_batch_per_e) < val_loss_to_beat:
+                    val_loss_to_beat = np.mean(val_losses_batch_per_e)
+                    # save the model
+                    model_path = f"{dir_to_save}/.{idd}_fold{fold}.pt"
+                    torch.save(dense_net, model_path)
+                    best_epoch = epoch
+
+                if early_stop:
+                    early_stopping(np.mean(val_losses_batch_per_e))
+
+            if early_stopping.early_stop:
+                break
+
+        print(f"Done training fold {fold}. Best val loss {val_loss_to_beat} at epoch {best_epoch}.")
+        if visualize:
+            fig, ax = plt.subplots(1, 1, figsize=(7, 5))
+            ax.plot(epochs, losses)
+            ax.plot(epochs, losses_val, label = "val")
+            ax.legend()
+            ax.set_xlabel("Epoch")
+            ax.set_ylabel("Loss")
+            ax.set_title(f"{idd}_fold{fold}")
+            fig.show()
+
+        # evaluate
+        fold_best_val_losses.append(val_loss_to_beat)
+               
+    
+    # load in the model / fold with the best val loss 
+    best_model_index = np.argmin(fold_best_val_losses)
+    best_model_path = f"{dir_to_save}/.{idd}_fold{best_model_index}.pt"
+    print(f"Loading in best model for {best_model_path}, val loss {np.min(fold_best_val_losses)} from fold {best_model_index}")
+    
+    dense_net_eval = torch.load(best_model_path)
+    dense_net_eval.eval()
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+        outputs = dense_net_eval(X_test).detach().cpu().numpy()
+        predicted = np.round(outputs)
+
+        # calculate auc 
+        auc = roc_auc_score(y_test, outputs)
+        fpr, tpr, _ = roc_curve(y_test, outputs)
+
+    if visualize:
+        fig, ax = plt.subplots(1, 1, figsize=(7, 5))
+        ax.plot(fpr, tpr)
+        ax.set_xlabel("FPR")
+        ax.set_ylabel("TPR")
+        ax.set_title("ROC: " + str(auc))
+        fname = f"{dir_to_save}/roc_{idd}"
+        fig.savefig(fname)
+        
+    np.save(f"{dir_to_save}/fpr_{idd}", fpr)
+    np.save(f"{dir_to_save}/tpr_{idd}", tpr)
+        
+    if auc < 0.5:
+        auc = 1.0 - auc
+    
+    return auc
+
+
 
 
 def analyze_band_transforms_CURTAILS_old(band, idd, transformed_sim_samples, dat_samples, classif_dir, n_epochs, batch_size, lr, visualize = False, test_samp1 = False, test_samp2 = False):
